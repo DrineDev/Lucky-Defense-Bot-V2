1458d1499164d542b1712437efbbfd92cbdd475e
Edwell John Cotejar <ejcotejar@gmail.com>
2024-10-08 22:23:52 +0800

    Improved openEmulator

--- src/Emulator/EmulatorBasic.java
+++ src/Emulator/EmulatorBasic.java
@@ -6,7 +6,10 @@ public class EmulatorBasic {
 
     public static void openEmulator() { // open emulator
         try {
-            Process process = Runtime.getRuntime().exec("C:/Program Files/Netease/MuMuPlayerGlobal-12.0/shell/MuMuPlayer.exe");
+            Process process;
+            process = Runtime.getRuntime().exec("adb connect 127.0.0.1:7555");
+            process = Runtime.getRuntime().exec("C:/Program Files/Netease/MuMuPlayerGlobal-12.0/shell/MuMuPlayer.exe");
+            process = Runtime.getRuntime().exec("adb shell wm size 540x960");
         } catch (IOException i) {
             System.out.println("Process opening failed.");
         }
